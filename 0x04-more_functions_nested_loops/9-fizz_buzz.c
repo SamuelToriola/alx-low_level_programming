125ed9551245fd83ce3335d20228bc6abecea6dc
SamuelToriola <sam.timitori@gmail.com>
2022-04-14 19:35:39 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+nes (36 sloc)  379 Bytes
+   
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+		printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
