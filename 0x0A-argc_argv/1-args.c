2a4885043a54ac6313c8f41b761016818e9358b8
SamuelToriola <sam.timitori@gmail.com>
2022-05-09 16:56:14 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+lines (13 sloc)  254 Bytes
+   
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print function name.
+ * @argc: number of arg in cl
+ * @argv: array of arg in cl
+ * Return: 0
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
