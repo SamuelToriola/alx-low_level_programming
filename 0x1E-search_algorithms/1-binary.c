f3e61ef25f9be0191b184a64c0d7e8937b4e6a30
Samuel Toriola <85284847+SamuelToriola@users.noreply.github.com>
2022-11-03 01:07:49 +0000

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,10 +1,14 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints the contents of an array.
+ * Prints the contents of an array.
  * @array: The source of the array to print.
  * @l: The left index of the array.
  * @r: The right index of the array.
+ * binary_search - Searches a value in a sorted array using a binary search.
+ * @array: The array to search in.
+ * @size: The length of the array.
+ * @value: The value to look for
  */
 void print_array(int *array, size_t l, size_t r)
 {
@@ -18,16 +22,6 @@ void print_array(int *array, size_t l, size_t r)
 	}
 }
 
-/**
- * binary_search_index - Searches a value in a sorted array using \
- * a binary search.
- * @array: The array to search in.
- * @l: The left index of the array.
- * @r: The right index of the array.
- * @value: The value to look for.
- *
- * Return: The first index of the value in the array, otherwise -1.
- */
 int binary_search_index(int *array, size_t l, size_t r, int value)
 {
 	size_t m;
@@ -46,14 +40,6 @@ int binary_search_index(int *array, size_t l, size_t r, int value)
 		return (binary_search_index(array, m + 1, r, value));
 }
 
-/**
- * binary_search - Searches a value in a sorted array using a binary search.
- * @array: The array to search in.
- * @size: The length of the array.
- * @value: The value to look for.
- *
- * Return: The index of the value in the array, otherwise -1.
- */
 int binary_search(int *array, size_t size, int value)
 {
 	return (binary_search_index(array, 0, size - 1, value));
